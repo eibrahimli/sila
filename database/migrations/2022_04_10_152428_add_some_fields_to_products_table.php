090eb50da8a13618838ebef9c19d80404407e4bc
elvir <ibrahimlielvir@gmail.com>
2022-04-10 15:26:25 +0400

    Products store_id added migrations

---
+++ database/migrations/2022_04_10_152428_add_some_fields_to_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSomeFieldsToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('store_id')->nullable();
+
+            $table->foreign('store_id')->references('id')->on('stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('store_id');
+            $table->dropColumn(['store_id']);
+        });
+    }
+}
